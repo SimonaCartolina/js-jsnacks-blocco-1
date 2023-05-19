d522ef39b3c41438f9b7f1e30378ff25d254bb46
Simona Cartolina <126583566+SimonaCartolina@users.noreply.github.com>
2023-05-19 11:17:03 +0200

    corrected snack 4 but still to finish

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /*Chiedi un numero di 4 cifre all’utente*/
+let userNumber= prompt(' Please insert a 4 digit number');
 
-
-const userNumber= parseInt(prompt('Insert a 4 digits number'));
+if(!(userNumber> 999 && userNumber< 9999)){
+    userNumber= prompt('Please insert a 4 digit number');
+}
 /*calcola la somma di tutte le cifre che compongono il numero.*/
 
 let sum=0;
 
-for(i=0; i<userNumber.lenght; i= i+1){
-    sum= sum+ userNumber;
-    onkeyup=sum(userNumber[i] + userNumber [i]);
-    console.log(sum);
+
+for(i=0; i < userNumber.length; i= i + 1){
+        sum= (userNumber.totringcharAt(i)) + sum;
+        console.log(sum);
 }
